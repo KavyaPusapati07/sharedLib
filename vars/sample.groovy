e35c6ebb8033b6e8329f65a1fceb327428169599
KavyaPusapati07 <90041283+KavyaPusapati07@users.noreply.github.com>
2022-01-21 16:19:36 +0530

    Update sample.groovy

--- vars/sample.groovy
+++ vars/sample.groovy
@@ -10,23 +10,17 @@ url: "${repoUrl}"
 }
 stage('Compile Stage'){
 steps{
-withMaven(maven: 'Maven3'){
 bat 'mvn clean compile'
-}
 }
 }
 stage('Test Stage'){
 steps{
-withMaven(maven: 'Maven3'){
 bat 'mvn test'
-}
 }
 }
 stage('Build Stage'){
 steps{
-withMaven(maven: 'Maven3'){
 bat 'mvn install'
-}
 }
 }
 }
